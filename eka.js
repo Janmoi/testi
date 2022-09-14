a2d7cbff400f760f1ff0695080d004c66f32f1d3
Jani Moilanen <c1moja00@students.oamk.fi>
2022-09-14 18:28:16 +0300

    mika taa onm

--- eka.js
+++ eka.js
@@ -1,6 +1,6 @@
 console.log("eka sovellus");
 console.log("terve saanan");
-for(var x=1; x<5; x++=){
+for(var x=1; x<5; x++=0){
     console.log("rivi "+x);
 }
 console.log("loopin jälkeen x="+x)
---
+++ toka.js
@@ -0,0 +1,9 @@
+function sayHello(){
+    console.log("hello from function");
+}
+
+function greetings(let fname){
+    console.log("hello "+fname);
+}
+sayHello();
+greetings("jussi");
